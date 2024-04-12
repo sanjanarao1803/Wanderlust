3614daa467fdd326173dfb61a81c9e50ed3188d9
Sanjana Rao <sanjanarao1803@gmail.com>
2024-04-12 15:20:18 +0530

    phase 1 part c - validations

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const ejsMate = require("ejs-mate");
 
 const Listing = require("./models/listing.js"); //requiring listing collection
 
+//wrapAsync
+const wrapAsync = require("./utils/wrapAsync.js");
+//ExpressError
+const ExpressError = require("./utils/ExpressError.js");
+
 //setting ejs
 const path=require("path");
 //parsing
@@ -21,6 +26,9 @@ app.engine('ejs',ejsMate);
 
 //static file - public folder
 app.use(express.static(path.join(__dirname,"/public")));
+
+//joi validate schema
+const {listingSchema} = require("./schema.js");
 
 //connecting with wanderlust db
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
@@ -40,11 +48,22 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
+//joi middleware
+const validateListing = (req,res,next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    } else{
+        next();
+    }
+}
+
 //index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 
 
@@ -54,45 +73,77 @@ app.get("/listings/new", (req, res) => {
 });
 
 //create route
-app.post("/listings",async (req,res)=>{
-    let listing=req.body.listing;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+// app.post("/listings",async (req,res)=>{
+//     let listing=req.body.listing;
+//     const newListing = new Listing(req.body.listing);
+//     await newListing.save();
+//     res.redirect("/listings");
+// });
+//with error handling
+// app.post("/listings",async (req,res,next)=>{
+//     try{
+//         // let listing=req.body.listing;
+//         const newListing = new Listing(req.body.listing);
+//         await newListing.save();
+//         res.redirect("/listings");    
+//     }catch(err){
+//         next(err);
+//     }
+// });
+//wrapAsync error handling
+app.post(
+    "/listings",
+    validateListing,
+    wrapAsync(async (req,res,next)=>{
+        //try using hoppscotch.io and send null data
+        // if(!req.body.listing){
+        //     throw new ExpressError(400,"Send valid data for listing");
+        // }
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings"); 
+    })   
+);
+
 
 //delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 
 //update route
-app.put("/listings/:id", async(req,res)=>{
+app.put(
+    "/listings/:id", 
+    validateListing,
+    wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing");
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     // console.log("sa");
     // res.redirect("/listings");
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //edit route
-app.get("/listings/:id/edit", async(req,res)=>{
+app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 
 
 //show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
+}));
 
 //basic route
 app.get("/",(req,res)=>{
@@ -114,6 +165,21 @@ app.get("/",(req,res)=>{
 //     res.send("successful testing");
 // });
 
+//page not found error
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page not found"));
+});
+
+//error handling
+// app.use((err,req,res,next) => {
+//     res.send("Something went wrong!");
+// });
+//ExpressError handling
+app.use((err,req,res,next) => {
+    let {statusCode=500,message="Something went wrong"} = err;
+    res.status(statusCode).render("error.ejs",{err});
+    // res.status(statusCode).send(message);
+});
 
 app.listen(8080,()=>{
     console.log(`app listening at port 8080`);
