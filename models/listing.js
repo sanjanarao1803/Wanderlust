348e65968c96f78a15e327f83b473342bf99f17c
Sanjana Rao <sanjanarao1803@gmail.com>
2024-04-13 23:57:30 +0530

    made reviews part of listing schema

--- models/listing.js
+++ models/listing.js
@@ -15,6 +15,12 @@ const listingSchema = new Schema({
     price:Number,
     location:String,
     country:String,
+    review:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review",
+        },
+    ],
 });
 
 const Listing = mongoose.model("Listing",listingSchema);
