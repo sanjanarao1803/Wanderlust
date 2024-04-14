ee5ec2f72246a4cefcbfb229d91b20673cf9af85
Sanjana Rao <sanjanarao1803@gmail.com>
2024-04-15 00:02:42 +0530

    delete route for reviews

--- app.js
+++ app.js
@@ -173,6 +173,18 @@ app.post("/listings/:id/reviews" , validateReview, wrapAsync(async(req,res) => {
 
 }));
 
+//delete route
+app.delete(
+    "/listings/:id/reviews/:reviewId",
+    wrapAsync(async(req,res) => {
+        let {id,reviewId} = req.params;
+        await Listing.findByIdAndUpdate(id, {$pull:{reviews:reviewId}});
+        await Review.findByIdAndDelete(reviewId);
+        res.redirect(`/listings/${id}`);
+    })
+    
+);
+
 //basic route
 app.get("/",(req,res)=>{
     res.send("Hi, I am root");
