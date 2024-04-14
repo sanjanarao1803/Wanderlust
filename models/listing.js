6ddacf0679187ba657aa6d1f64870cbb40eda98c
Sanjana Rao <sanjanarao1803@gmail.com>
2024-04-15 00:14:54 +0530

    Reviews

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const Review = require("./review.js");
+
 const listingSchema = new Schema({
     title:{
         type:String,
@@ -21,6 +23,12 @@ const listingSchema = new Schema({
             ref:"Review",
         },
     ],
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing",listingSchema);
